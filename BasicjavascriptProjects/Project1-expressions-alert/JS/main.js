1f6477ad7d2b86078a5a2c7d4bc9f3577bfeb846
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-01-25 14:01:01 -0800

    edited js file

---
+++ BasicjavascriptProjects/Project1-expressions-alert/JS/main.js
@@ -0,0 +1,24 @@
+
+var A = "this is a string";
+window.alert(A);
+document.write("hello again");
+
+document.write(A);
+document.write("+lisa told bart, \"knock it off bart! or i'll tell dad\"");
+document.write("\"be who you are and say what you feel,"+" because those who mind don\'t matter and those who matter son\'t mind.\""+"DR seuss");
+
+var B = "concataenated"+" string"
+document.write(B)
+
+var Family = "The Arezz", Dad = "jerry", mom = "hermi",
+son = "zorro";
+document.write(son);
+document.write(3 + 3);
+
+
+
+function my_first_function() {
+    var str = "this is the button text!";
+    document.getElementById("button_text").innerHTML = str; //this is a comment
+
+}
