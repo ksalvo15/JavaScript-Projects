2a3bcb7cb9d819fb56af3d77e3d51f98e4bf8ada
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-01-26 11:40:06 -0800

    edited js file

--- BasicjavascriptProjects/Project1-expressions-alert/JS/main.js
+++ BasicjavascriptProjects/Project1-expressions-alert/JS/main.js
@@ -1,24 +1,29 @@
 
 var A = "this is a string";
 window.alert(A);
-document.write("hello again");
+//document.write("hello again");
 
-document.write(A);
-document.write("+lisa told bart, \"knock it off bart! or i'll tell dad\"");
-document.write("\"be who you are and say what you feel,"+" because those who mind don\'t matter and those who matter son\'t mind.\""+"DR seuss");
+//document.write(A);
+//document.write("+lisa told bart, \"knock it off bart! or i'll tell dad\"");
+//document.write("\"be who you are and say what you feel,"+" because those who mind don\'t matter and those who matter son\'t mind.\""+"DR seuss");
 
-var B = "concataenated"+" string"
-document.write(B)
+//var B = "concataenated"+" string"
+//document.write(B)
 
-var Family = "The Arezz", Dad = "jerry", mom = "hermi",
-son = "zorro";
-document.write(son);
-document.write(3 + 3);
+//var Family = "The Arezz", Dad = "jerry", mom = "hermi",
+//son = "zorro";
+//document.write(son);
+//document.write(3 + 3);
 
 
 
-function my_first_function() {
-    var str = "this is the button text!";
-    document.getElementById("button_text").innerHTML = str; //this is a comment
+//function my_first_function() {
+    //var str = "this is the button text!";
+   // document.getElementById("button_text").innerHTML = str; //this is a comment
+//}
 
-}
+
+var Sent1 = "This is the beginning of the string"; //this ia variable sent1
+var Sent2 = " and this is the end of the string"; //this ia variable sent2
+document.write(Sent1 + Sent2); //this is an expression
+document.write("This is the beginning of the string" + " and this is the end of the string");//this is a concatenateed string
