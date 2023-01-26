8e4db642d349289acd71c74a2cfb0366bf569992
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-01-26 13:38:13 -0800

    edited js for concatenate

---
+++ BasicjavascriptProjects/Project2/js/main.js
@@ -0,0 +1,11 @@
+
+function myfunction() {
+    var sentence = "i am learning a";
+    sentence += " lot of from this course";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+
+///function my_first_function() { //creating the button fuction
+   // var str = "this is the button text!";
+    //document.getElementById("button_text").innerHTML = str; 
+//}
