06e940079bf3d8770390069bedd3d817db05c8f5
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-01-31 12:35:35 -0800

    Update main.js

--- BasicjavascriptProjects/project7/js/main.js
+++ BasicjavascriptProjects/project7/js/main.js
@@ -1,20 +1,4 @@
-var x=10; //global variable
-var y = 20; //local variable
 
-function addnumbers1() {
-    document.write(20 + x +"<br>");
-}
-function addnumbers2() {
-    
-    document.write(x + y + 10 +"<br>");
-}
-function addnumbers3() {
-    console.log(x + y + 100 +"<br>"); //debug to se why the local variable y is not showing up
-}
-
-addnumbers1();
-addnumbers2();
-addnumbers3();
 
 if (1<2) {
     document.write("the left number is smaller thant the right");
@@ -41,3 +25,21 @@ function agefunction() {
     }
     document.getElementById("howold?").innerHTML = vote;
 }
+
+var x=10; //global variable
+var y = 20; //local variable
+
+function addnumbers1() {
+    document.write(20 + x +"<br>");
+}
+function addnumbers2() {
+    
+    document.write(x + y + 10 +"<br>");
+}
+function addnumbers3() {
+    console.log(x + y + 100 +"<br>"); //debug to se why the local variable y is not showing up
+}
+
+addnumbers1();
+addnumbers2();
+addnumbers3();
