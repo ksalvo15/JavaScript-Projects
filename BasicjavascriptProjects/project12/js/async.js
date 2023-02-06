4770ff0574088e57c460cff5677ac2a9a0db4a9b
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-02-06 11:06:59 -0800

    added folders

---
+++ BasicjavascriptProjects/project12/js/async.js
@@ -0,0 +1,5 @@
+alert(document.getElementById("pl").innerHTML);
+function myfunct() {
+    var fruits = ["bannana", " orange", " apple"];
+    document.getElementById("pl").innerHTML = fruits.valueOf();
+}
